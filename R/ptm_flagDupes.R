9f5e15074fe6cb722518549c4c1359b468064a09
Hassan <hejaziihassan@gmail.com>
2024-03-16 13:26:17 +0100

    change name of 'aa_countplot' function into 'seq_AAcountplot'. Add function `ptm_flagDupes` that mark redundant ptms with astresik.

---
+++ R/ptm_flagDupes.R
@@ -0,0 +1,132 @@
+
+
+
+
+#flag redundant isoforms having the same peak intensities.
+#' Flagging suspicious sequence-PTM combinations
+#' @description
+#' In Proline result output, some sequence-PTM combinations have identical intensity values in all samples.
+#' Usually those are isobaric PTM-sequence combinations that are most likely false positive hits.
+#'
+#'
+#' @param df A \code{dataframe}.
+#' @param PTM_col PTM column name in the dataset.
+#' @param var_col Column name of a unique identifier (e.g. peptide sequence) in the dataset.
+#' @param select_cols Either one of the select helper functions with proper argument
+#' (e.g. \code{starts_with("WT_")}) or a vector \code{c()} containing the names of
+#'  intensity/abundance columns to check for duplication.
+#'
+#' @import stringr
+#' @import dplyr
+#' @import rlang
+#' @import cli
+#'
+#' @return A named \code{list} of two \code{tibbles}.
+#'
+#' \emph{unique} \cr
+#' * a non-redundant version of \code{df} after:\cr
+#'     - removing identical duplicate rows \cr
+#'     - removing redundant rows sharing identical intensity values \cr
+#'     - marking with asterisk (*) redundant PTMs in \code{PTM_col}.\cr
+#'       Redundant observations are reported in \code{duplicate} (see below) with their frequency \code{n}.
+#'
+#' \emph{duplicate} \cr
+#' * A subset \code{tibble} of \code{df} containing all de-duplicated identifications sharing
+#'  identical intensities in \strong{ALL} \code{select_cols} columns.
+#'
+#' \emph{freq_table} \cr
+#' * A table summarizing the duplicate counts and their frequencies in the dataset.
+#'
+#' @export
+
+ptm_flagDupes <- function(df,PTM_col, var_col, select_cols) {
+
+  select_exp <- rlang::enexpr(select_cols)
+  PTM_col <-  rlang::enexpr(PTM_col)
+  var_col <-  rlang::enexpr(var_col)
+
+
+
+  # Record time
+  start_time <- Sys.time()
+
+  #remove IDENTICAL ROWS## could be same peptide from different proteins
+  NotIdentical <- df[!duplicated(df), ]
+  ##count the number of duplicate row(s) deleted##
+  nrows_ident <- df[duplicated(df), ] |> nrow()
+  cli::cli_alert_info('The number of removed identical rows is {nrows_ident}.')
+
+
+
+  #isolate different IDs but having same intensities over ALL samples.
+  Duplicates <- NotIdentical |>
+    dplyr::add_count(dplyr::across(!!select_exp)) |>
+    dplyr::filter(n > 1)
+
+  rlang::eval_tidy(Duplicates)
+
+
+  nrows_dup <- nrow(Duplicates)
+
+  cli::cli_alert_info('The number of duplicate intensity rows is {nrows_dup}.')
+
+
+  #get the frequence of duplicate counts
+freq_table <- table(Duplicates$n) |> as.data.frame()
+names(freq_table) <- c('dupes_count', 'freq')
+
+  #remove duplications where:
+  # the intensity value is identical for more than one ID (case of delta = 0)#
+  # identification is repeated (shared peptide between two or more proteins)#
+
+  uniqueHistoneForms <- NotIdentical |>
+    dplyr::distinct(.data = _, across(!!select_exp) , .keep_all = TRUE)
+
+  rlang::eval_tidy(uniqueHistoneForms)
+
+
+
+# Iterate over the rows of uniqueHistoneForms
+  for (i in seq_len(nrow(uniqueHistoneForms))) {
+
+
+    PTM_value <- uniqueHistoneForms[[PTM_col]][i]
+    var_value <- uniqueHistoneForms[[var_col]][i]
+
+    if (!is.na(PTM_value)) { #in case non-modified peptide
+
+      if(any(Duplicates[[PTM_col]] == PTM_value
+             &
+        Duplicates[[var_col]] == var_value)) {
+
+        uniqueHistoneForms[[PTM_col]][i] <- paste0(PTM_value, "*")
+     }
+    }
+  }
+
+
+
+#count number of marked (*) PTMs
+  num_marked_ptms = uniqueHistoneForms |>
+    dplyr::filter(stringr::str_detect(!!PTM_col, "\\*")) |> nrow()
+
+
+
+duration = round(as.numeric(Sys.time() - start_time), 1)
+  cli::cli_alert_success('
+                         {num_marked_ptms} PTMs are marked in {duration} s.
+                        ')
+
+
+cli::cli_alert_warning("\nAttention
+If the abundance values of two redundant identifications aren't exactly the same,
+the redundant row won't be deleted. These rare cases will go unnoticed and won't be reported.
+Please manually review for greater accuracy.")
+
+  return(list(unique = uniqueHistoneForms, duplicate = Duplicates, freq_table = freq_table ))
+}
+
+
+
+
+
---
+++ R/seq_AAcountplot.R
@@ -0,0 +1,63 @@
+
+
+
+
+#' Count and plot amino aicd occurence
+#' @description
+#' Count the occurence of amino aicd residues in a protein sequence and plot their relative percentages.
+#'
+#' @param ... Protein sequence as a character string.
+#' @param plot A \code{logical} argument to decide whether to plot or not the resuls.
+#' @import stringr
+#' @import dplyr
+#' @import purrr
+#' @import ggplot2
+#' @import scales
+#' @import tibble
+#' @return dataframe with 3 columns: name of a sequence, its amino acid residues and their count.
+#' a plot if \code{plot} argument is set to \code{TRUE} .
+#' @examples
+#'H3_seq <-  "ARTKQTARKSTGGKAPRKQLATKAARKSAPATGGVKKPHRYRPGTVALREIRRYQKSTELLIRKLPFQRLVREIAQDFKTDLRFQSSAVMALQEACEAYLVGLFEDTNLCAIHAKRVTIMPKDIQLARRIRGERA"
+#'H4_seq <-  "SGRGKGGKGLGKGGAKRHRKVLRDNIQGITKPAIRRLARRGGVKRISGLIYEETRGVLKVFLENVIRDAVTYTEHAKRKTVTAMDVVYALKRQGRTLYGFGG"
+#'seq_AAcountplot(H3_seq)
+#'seq_AAcountplot(H3_seq, H4_seq, plot = TRUE)
+#' @export
+seq_AAcountplot <- function(..., plot = FALSE) {
+
+seq_list <- dplyr::lst(...)
+
+SEQsplit  <- purrr::map(unlist(seq_list), ~stringr::str_split_1(., pattern = ""))
+
+SEQunique <- purrr::map(SEQsplit, ~stringr::str_unique(.))
+
+df_count  <-  purrr::map2(.x = seq_list,
+                          .y = SEQunique,
+                          .f = ~ tibble::tibble(a.a = .y,
+                                        count = round(stringr::str_count(string = .x ,
+                                                                         pattern = .y)/nchar(.x), 2)*100
+
+                          )) |> dplyr::bind_rows( .id= "seq")
+
+
+
+if(plot == TRUE){
+
+  plot_aa_freq <- df_count |>
+    ggplot2::ggplot(ggplot2::aes(x= a.a, y= count, fill= seq)) +
+    ggplot2::geom_col(position = ggplot2::position_dodge2(width  = 0.2, preserve = "single"))+
+    ggplot2::labs(y= 'Relative Percentage (%)')+
+    ggplot2::scale_fill_brewer(palette = "Dark2")+
+    ggplot2::scale_y_continuous(labels = scales::percent_format(scale = 1))+
+    ggplot2::theme_minimal(base_size = 14)
+
+  print(plot_aa_freq)
+
+
+}
+
+return(df_count)
+
+}
+
+
+#'
