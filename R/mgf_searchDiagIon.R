279fb8a24c92a585c7f794c5895ca8d1c97f6049
Hassan <hejaziihassan@gmail.com>
2024-04-22 00:40:51 +0200

    add checks to ensure `BiocManager`, `Spectra`, and `MsBackendMgf` packages are installed. Move return function to end of the code to allow testing if `save_file` argument.

--- R/mgf_searchDiagIon.R
+++ R/mgf_searchDiagIon.R
@@ -3,7 +3,7 @@
 
 .mgf_to_sp <- function(mgf_file){
   cli::cli_inform(message = "Converting {mgf_file} file into Spectra object")
-  BiocParallel::register(BiocParallel::SerialParam())
+  BiocParallel::register(BPPARAM = BiocParallel::SerialParam())
   Spectra::Spectra(object = {{mgf_file}}, source = MsBackendMgf::MsBackendMgf())
 
 }
@@ -21,13 +21,41 @@
 #' @return A \code{tibble} with 6 columns including the diagnostic ion \code{m/z} and its intensity relative to the base peak.
 #' @import dplyr
 #' @import Spectra
-#' @import BiocParallel
-#' @import MsBackendMgf
+#' @importFrom MsBackendMgf MsBackendMgf
+#' @import Spectra
 #' @import cli
+#' @import rlang
 #' @export
 mgf_searchDiagIon <- function(mgf_file, diag_ion, tol = 0.002, save_file = FALSE){
 
-  file_name = basename(stringr::str_remove(string = mgf_file, pattern = ".mgf"))
+  libraries <- c("BiocParallel", "Spectra", "MsBackendMgf")
+
+  if (!require("BiocManager")) install.packages("BiocManager")
+
+  #Checking if the package belongs to CRAN or to Bioconductor and installing them accordingly.
+
+  for(lib in libraries){
+    if(!lib %in% installed.packages()){
+      if(lib %in% available.packages()[,1]){
+        install.packages(lib,dependencies=TRUE)
+      }else {(BiocManager::install(lib))
+      }}
+  }
+
+  #Loading the libraries
+  sapply(libraries,require,character=TRUE)
+
+
+if(is_missing(mgf_file)){
+  cli::cli_abort("No mgf file name is identified.")
+}
+if(is_missing(diag_ion)){
+  cli::cli_abort("No diagnostic ion is identified.")
+}
+
+
+
+  file_name = tools::file_path_sans_ext(basename(mgf_file))
 
   sps <- .mgf_to_sp(mgf_file)
 
@@ -37,15 +65,15 @@ mgf_searchDiagIon <- function(mgf_file, diag_ion, tol = 0.002, save_file = FALSE
 
 cli::cli_inform(message = "Extracting diagnostic ions from {mgf_file}.")
 
-cli::cli_progress_bar()
+cli::cli_progress_bar(type = "iterator")
 
-  for (i in 1:length(acquisitionNum(sps))) {
+  for (i in 1:length(Spectra::acquisitionNum(sps))) {
     temp_list_combined <- list()
 
 
     for (ion in seq_along(diag_ion)) {
-      isIonhere <-
-        dplyr::near(diag_ion[ion], mz(sps)[[i]], tol = tol) #logical vector
+
+      isIonhere <- dplyr::near(diag_ion[ion], mz(sps)[[i]], tol = tol) #logical vector
 
       #find at least one is TRUE
       if (isTRUE(any(isIonhere, na.rm = TRUE))) {
@@ -60,7 +88,7 @@ cli::cli_progress_bar()
             diag_ion     = round(Spectra::mz(sps)[[i]][indices[indx]], 4),
             diag_relint  = round(Spectra::intensity(sps)[[i]][indices[indx]] / max(Spectra::intensity(sps)[[i]]),3),
             scan         = Spectra::acquisitionNum(sps)[[i]],
-            prec_mass    = round(Spectra::precursorMz(sps)[[i]], 4),
+            prec_mz      = round(Spectra::precursorMz(sps)[[i]], 4),
             prec_z       = Spectra::precursorCharge(sps)[[i]],
             rt           = round(Spectra::rtime(sps)[[i]] / 60, 2) #sec to min
           )
@@ -82,21 +110,34 @@ cli::cli_progress_bar()
     final_list[[i]] <- dplyr::bind_rows(temp_list_combined)
   }
 
-  final_df <- dplyr::bind_rows(final_list) |>
+  final_df <- dplyr::bind_rows(final_list)
+
+  if(nrow( final_df) == 0L){cli::cli_inform("No diagnostic ion was found.")}else{
+
+    final_df <- final_df |>
     dplyr::mutate(
-      dplyr::across(dplyr::all_of(c("diag_ion", "diag_relint", "prec_mass", "rt")), as.double),
+      dplyr::across(dplyr::all_of(c("diag_ion", "diag_relint", "prec_mz", "rt")), as.double),
       dplyr::across(dplyr::all_of(c("scan", "prec_z")), as.integer)
     )
 
-#cli::cli_progress_done()
 
-  return(final_df)
 
-  if(isTRUE(save_file)){
-    file_csv = paste0("diagIons_", file_name, ".csv")
-    write.csv(x = df, file = file_csv, row.names = FALSE)
-    cli::cli_alert_success("{file_name} file is saved sucessfully.")
-  }
+    if(isTRUE(save_file)){
+      file_csv = paste0("diagIons_", file_name, ".csv")
+      write.csv(x = final_df, file = file_csv, row.names = FALSE)
+      cli::cli_alert_success("{file_csv} file is saved sucessfully.")
+    }
+
+    return(final_df)
+
+}
+cli::cli_progress_done(result = "done")
+
+
+
+
+
+
 
 }
 
