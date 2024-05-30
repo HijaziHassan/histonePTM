ec087df1beb6cf50f99f59f1dc9c93cd85989b7b
Hassan HIJAZI <hejaziihassan@gmail.com>
2024-05-30 18:04:53 +0200

    ad supress warning function before the library installation.

--- tests/testthat.R
+++ tests/testthat.R
@@ -6,7 +6,7 @@
 # * https://r-pkgs.org/testing-design.html#sec-tests-files-overview
 # * https://testthat.r-lib.org/articles/special-files.html
 
-library(testthat)
-library(histonePTM)
+suppressWarnings(library(testthat))
+suppressPackageStartupMessages(library(histonePTM))
 
 test_check("histonePTM")
