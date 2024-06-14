1c31a1b68ae2ebc622e9b63b2e7ae43df0ba9bea
Hassan HIJAZI <hejaziihassan@gmail.com>
2024-06-14 18:08:48 +0200

    update documentation.

--- R/misc_checkFileNames.R
+++ R/misc_checkFileNames.R
@@ -1,4 +1,21 @@
-# Check file names provided with those actually found in the Proline excel output.
+#' Check match betwen two columns
+#' @description
+#' Could be used to check for user input file names if they match file names extracted from 'Search settings and infos' sheet of Proline export.
+#'
+#' @param raw_df Dataframe containing acolumn of correct file names
+#' @param user_df Dataframe containing a column of file names provided by the user to which another custom name are assigned
+#' @param common_col column of file names in both dataframes (should be the same name in both).
+#'
+#' @return error or success message.
+#' @export
+#' @examples
+#' df1 <- data.frame(file= c('HF1_022210', 'HF1_022222'))
+#' df2 <- df1
+#' misc_checkFileNames(df1, df2, common_col = 'file')
+#' #>The provided and actual names match perfectly. Good job!✔
+
+
+
 misc_checkFileNames <- function(raw_df, user_df, common_col) {
   check.presence = all(raw_df[[common_col]] %in% user_df[[common_col]])
   check.extras = all.equal(sort(raw_df[[common_col]]), sort(user_df[[common_col]]))
@@ -12,5 +29,5 @@ misc_checkFileNames <- function(raw_df, user_df, common_col) {
 
   if(isTRUE(check.extras)){
     cli::cli_alert_success(cat("\nThe provided and actual names match perfectly. Good job!"))
-  }else{cli::cli_alert_warning(cat("\nExtra unmatched names are dropped"))}
+  }else{cli::cli_alert_warning(cat("\nThere are extra unmatched names."))}
 }
