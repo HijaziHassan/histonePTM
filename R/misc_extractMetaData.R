7349d32582027053ae289deeb16aac85879874fe
Hassan HIJAZI <hejaziihassan@gmail.com>
2024-06-20 15:14:22 +0200

    add to the `as_tibble` function the argument `.name_repair` and rename the columns after. Replace magrittr pipe with the native one.

--- R/misc_extractMetaData.R
+++ R/misc_extractMetaData.R
@@ -19,15 +19,18 @@ misc_extractMetaData <- function(filename, save_file= ""){
 
     extract_names <- readxl::read_excel(path = {{filename}},
                                         sheet = "Search settings and infos",
-                                        .name_repair = "unique_quiet") %>%
+                                        .name_repair = "unique_quiet") |>
       #I used those row identifiers since they'are always present even if one sample is being processed in Proline.
-      dplyr::filter(project_name %in% c("result_file_name", "raw_file_name", "job_number")) %>%
+      dplyr::filter(project_name %in% c("result_file_name", "raw_file_name", "job_number")) |>
       tibble::as_tibble()
 
   })
 
-  dat_files <- tibble::as_tibble(t(extract_names[ , - 1])) %>%
-    dplyr::rename(file = V1, channel = V2, dat = V3) %>%
+  dat_files <- tibble::as_tibble(t(extract_names[ , - 1]), .name_repair = "minimal")
+
+  colnames(dat_files) = c('file', 'channel', 'dat')
+
+  dat_files <- dat_files |>
     dplyr::mutate(dat= as.integer(dat)) |>
     dplyr::mutate(channel= stringr::str_remove(channel, ".dat|.pep.xml|.pepXML|.msf|.pdResult|.mzid"))
 
