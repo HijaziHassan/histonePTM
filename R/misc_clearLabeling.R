cf1cc12450bf733e5f18899136c02a50783bc3e9
Hassan HIJAZI <hejaziihassan@gmail.com>
2024-06-20 14:51:12 +0200

    a function to replace derivatization annotation in PTM string such as propionyl by more biologically relevant annotation.

---
+++ R/misc_clearLabeling.R
@@ -0,0 +1,40 @@
+
+
+
+
+#' Reverse PTM Labeling
+#'
+#' For final representation it is better to replace chemical labeling with biological state. For example, replace 'propionyl' with 'unmodified'
+#' or 'butyryl' with 'me1' (since me1+pr = bu).
+#'
+#' @param ptm_string PTM-containing string
+#' @param rules A named vector containing \code{(labeled_ptm = unlabeled_ptm)}. Example: \code{"pr" = "un"}.
+#'
+#' @return unlabled string
+#' @export
+#'
+
+misc_clearLabeling <- function(ptm_string, rules){
+
+  if(is_missing(rules)){
+  rules = c(".+Nt-" = "", #remove N-terminal Labeling
+            "pr"="un", #replace propionyl with unmod
+            "bu"="me1", #replace bu which is me1-pr with me1
+            "tmame1" = "me1", #replace tmame1 with me1
+            "tma" = "un" #replace TMAyl with unmod
+  )
+  }
+
+  if(stringr::str_detect(ptm_string, "me1\\b") & "me1" %in% unname(rules)){
+    cli::cli_abort(c('You are trying to replace "bu" by "me1" but "me1" is already in the rules.',
+                "i" = "remove '{ptm_string}' and all ptm strings containing 'me1' then apply the function again."))
+  }
+
+ stringr::str_replace_all(ptm_string, rules)
+
+}
+
+
+
+
+
