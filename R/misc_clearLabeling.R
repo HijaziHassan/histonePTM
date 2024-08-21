de887ced39c4beabc1b7543c37947b527f1a4833
Hassan HIJAZI <hejaziihassan@gmail.com>
2024-08-21 23:19:56 +0200

    "update abort message to consider other forms of PTM nomenclature (e.g. K27me1-K37me2 ...) and not only me1-me2 ...

--- R/misc_clearLabeling.R
+++ R/misc_clearLabeling.R
@@ -10,24 +10,25 @@
 #' @param ptm_string PTM-containing string
 #' @param rules A named vector containing \code{(labeled_ptm = unlabeled_ptm)}. Example: \code{"pr" = "un"}.
 #'
-#' @return unlabled string
+#' @return unlabeled string
 #' @export
 #'
 
 misc_clearLabeling <- function(ptm_string, rules){
 
   if(is_missing(rules)){
-  rules = c(".+Nt-" = "", #remove N-terminal Labeling
+  rules = c(".+Nt-?" = "", #remove N-terminal Labeling
             "pr"="un", #replace propionyl with unmod
             "bu"="me1", #replace bu which is me1-pr with me1
             "tmame1" = "me1", #replace tmame1 with me1
             "tma" = "un" #replace TMAyl with unmod
   )
+
+  if(any(stringr::str_detect(ptm_string, "(?:[:upper:]*\\d*me1)\\b")) & "me1" %in% unname(rules)){
+    cli::cli_abort(c('You are trying to replace "bu" by "me1" but "me1" is already in your `rules`.',
+                     "i" = "remove any 'me1' occurnce in `ptm_string` before applying the function or change the `rules` argument."))
   }
 
-  if(any(stringr::str_detect(ptm_string, "me1\\b")) & "me1" %in% unname(rules)){
-    cli::cli_abort(c('You are trying to replace "bu" by "me1" but "me1" is already in the rules.',
-                "i" = "remove any 'me1' occurnce in {ptm_string}' before applying the function or change the 'rules' argument."))
   }
 
 
