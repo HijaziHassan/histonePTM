c3f7433a9152e8c2ead68eff0f924086c2807bb8
Hassan HIJAZI <hejaziihassan@gmail.com>
2024-08-21 23:58:03 +0200

    account for different label types at y-axis (it could be character or numeric "

--- R/plot_scheduledPRMranges.R
+++ R/plot_scheduledPRMranges.R
@@ -17,7 +17,8 @@
 plot_schdueldPRMranges <- function(df,tr_start, tr_end, y_axis,label, save_plot = FALSE, ...){
 
 
-ggplot2::ggplot(df, ggplot2::aes(xmin = {{tr_start}},
+
+p <- ggplot2::ggplot(df, ggplot2::aes(xmin = {{tr_start}},
                                 xmax = {{tr_end}},
                                 x= ({{tr_start}} + {{tr_end}})/2,
                                 y = reorder(factor({{y_axis}}), {{tr_end}}),
@@ -28,10 +29,10 @@ ggplot2::ggplot(df, ggplot2::aes(xmin = {{tr_start}},
 
 ggplot2::geom_linerange(linewidth = 4 , alpha = 0.5, colour = "#FFD21F",
                         position = ggplot2::position_dodge(width = 0.5)) +
-    ggplot2::geom_text(ggplot2::aes(label = round({{label}}, 2)), size = 2, color = "black")+
-    ggplot2::scale_x_continuous(breaks = seq(0,
-                                             floor(max(df |> dplyr::pull({{y_axis}}))),
-                                             5)) +
+    ggplot2::geom_text(ggplot2::aes(label = if (is.numeric({{label}})) round({{label}}, 2) else {{label}}), size = 3.5, color = "black")+
+    # ggplot2::scale_x_continuous(breaks = seq(0,
+    #                                          floor(max(df |> dplyr::pull(tr_end))),
+    #                                          5)) +
 #ggplot2::scale_color_viridis_d() +
 ggplot2::labs(y= "", x= " Retention time (min)")+
 ggplot2::theme_classic(base_size = 14)+
@@ -43,14 +44,14 @@ ggplot2::theme(legend.title = ggplot2::element_blank(),
                axis.line.y = ggplot2::element_blank(),
                axis.line.x = ggplot2::element_line(linewidth = 1.25),
                                                           ...)
-
-  return(plot)
-
-  if(isTRUE(save_plot)){
+ if(isTRUE(save_plot)){
 
     ggplot2::ggsave("prm_scheduled_ranges.png", height = 10, width =7, dpi = 300)
     cli::cli_alert_success(text = "Scheduled PRM range plot is saved successfully.")
   }
+
+  return(p)
+
 
 }
 
