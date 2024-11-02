39fd6244b41be0fa2188633210d6842ffe038b0b
HIJAZI Hassan 280913 <HH280913@intra.cea.fr>
2024-11-02 19:16:08 +0100

    add argument `norm_method` to `quant_relIntensity()` which allows to normalize by the total intensity of peptides per sample or by total intensity of peptide family as before (default).

--- R/quant_relIntensity.R
+++ R/quant_relIntensity.R
@@ -8,12 +8,14 @@
 #' @param select_cols Either one of the select helper functions with proper argument
 #' (e.g. \code{starts_with("WT_")}) or a vector \code{c()}containing the names of
 #'  intensity/abundance columns to be normalized.
-#' @param grouping_var A unqiue variable to group by like 'peptide sequence' or so.
+#' @param grouping_var A unqiue variable to group by like 'peptide sequence' or so. This mandatory if `norm_method` is 'peptide_family".
+#' @param norm_method Normalization method. Either by peptide family or by total intensity of all (histone) peptides in the sample. The latter depends on what is in your dataset and if you have prefiltered it or not.
 #'
 #' @return The provided \code{dataframe} is returned with relative instead of absolute
 #' intensity values of the abundance/intensity columns passed to the function.
 #'
-#' \deqn{\text{Relative Intensity} = \frac{\text{Intensity}_{\text{peptide X}}}{\sum \text{Intensity}_{\text{all peptides of the same sequence as X}}}}
+#' \deqn{\text{norm_peptide_family} = \frac{\text{Intensity}_{\text{peptide X}}}{\sum \text{Intensity}_{\text{all peptides of the same sequence as X}}}}
+#' \deqn{\text{norm_peptide_family} = \frac{\text{Intensity}_{\text{peptide X}}}{\sum \text{Intensity}_{\text{all peptides in the sample}}}}
 #'
 #'
 #' @examples
@@ -24,14 +26,24 @@
 #'
 #' @export
 
-quant_relIntensity <- function(df, select_cols, grouping_var){
+quant_relIntensity <- function(df, select_cols, grouping_var, norm_method = c('peptide_family', "total_histone")){
+
+  norm_by = match.arg(norm_method)
+
+
+  if ( norm_by == 'peptide_family'){
 
   df_norm <- df |>
     dplyr::mutate(dplyr::across({{select_cols}}, ~ .normalize_vec(x=.)),
                    .by = {{grouping_var}}
     )
 
+  }else if(norm_by =="total_histone" ){
 
+    df_norm <- df |>
+      dplyr::mutate(dplyr::across({{select_cols}}, ~ .normalize_vec(x=.)))
+
+}
 
   return(df_norm)
 
