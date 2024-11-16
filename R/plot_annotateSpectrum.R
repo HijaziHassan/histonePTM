f161a6a8720150c6fc0ec3e343ae409f082b1346
HIJAZI Hassan 280913 <HH280913@intra.cea.fr>
2024-11-16 15:46:31 +0100

    support annotation to diagnostic ions use custom annotation function and enhance aesthetics.

--- R/plot_annotateSpectrum.R
+++ R/plot_annotateSpectrum.R
@@ -1,25 +1,33 @@
 
-
-
-
 #' Annotate MS/MS Spectra
 #' @description
 #' wrapper function around functions from \code{spectrum_utils} library in Python.
 #'
 #' @param Profrma_peptide Peptide in proforma representation
-#' @param prec_mz m/z of the precursor ion
-#' @param prec_z charge state of the precursor ion
+#' @param prec_mz m/z of the precursor ion (double)
+#' @param prec_z charge state of the precursor ion (integer)
 #' @param mz \code{numeric} vector containing mz values
 #' @param intensity \code{numeric} vector containing intensity values
+#' @param nloss (bool, TRUE (default)). Annotate neutral losses (-NH3 by "*" and -H2O by "o")
+#' @param annot_diag (bool, FALSE (default)) Annotate CyIm ions produced from modified lysines.
+#' @param ion_types Fragment ion types (i.e. "by' (Default))
+#' @param tol_mz fragment mass tolerance (e.g, 20)
+#' @param tol_mode 'ppm' (Default) or 'Da'
 #' @param title title of the annotate MS/MS plot
 #' @param output_plot_name Name of the plot in case you want to save it
 #'
-#' @return Annotate MS/MS plot
+#' @return Annotated MS/MS plot
 #' @export
 plot_annotateSpectrum <- function(Profrma_peptide,
-                                  prec_mz, prec_z,
+                                  prec_mz,
+                                  prec_z,
                                   mz, intensity,
                                   title= "",
+                                  annot_diag= FALSE,
+                                  nloss= TRUE,
+                                  ion_types= "by",
+                                  tol_mz = "20",
+                                  tol_mode = "ppm",
                                   output_plot_name= ""){
 
 
@@ -27,16 +35,30 @@ plot_annotateSpectrum <- function(Profrma_peptide,
     stop("Package 'reticulate' is required but not installed.")
   }
 
-reticulate::source_python("./inst/python/plot_annotateSpectra.py")
+  My_Package_Name_path <- system.file("python", package = "histonePTM")
+  reticulate::py_run_string(paste0("import sys; sys.path.append('", My_Package_Name_path, "')"))
+
+  reticulate::source_python(system.file("python",
+                                        "plot_annotateSpectra.py",
+                                        package = "histonePTM",
+                                        mustWork = TRUE
+  ))
 
 
-plot_annotateSpectra(identifier= title,
-                       peptide= Profrma_peptide,
-                       precursor_mz= prec_mz,
-                       precursor_charge = prec_z,
-                       mz = mz,
-                       intensity = intensity,
-                       output_plot_name = output_plot_name)
+  plot_annotateSpectra(
+    identifier = title,
+    peptide = Profrma_peptide,
+    precursor_mz = prec_mz,
+    precursor_charge = prec_z,
+    mz = mz,
+    intensity = intensity,
+    nloss = nloss,
+    annot_diag = annot_diag,
+    ion_types = ion_types,
+    tol_mz = tol_mz,
+    tol_mode = tol_mode,
+    output_plot_name = output_plot_name
+  )
 }
 
 
--- inst/python/plot_annotateSpectra.py
+++ inst/python/plot_annotateSpectra.py
@@ -1,53 +1,147 @@
-
+import functools
 import matplotlib.pyplot as plt
 import spectrum_utils.plot as sup
 import spectrum_utils.spectrum as sus
+import spectrum_utils.fragment_annotation as sufa
+import spectrum_utils.utils as suu
 
 
+# function for custom annotation
+#1. If precursor charge is 2, annotate with only fragment type with no (+)
+#2. Option to or no include H2O and NH3 neutral losses
+#3. Option to or no include diagnostic ions
+def annotate_ion_type(annotation, prec_z, ion_types, annot_diag = False, neutraloss=False):
 
-
-
-annotation_settings = {
-    "fragment_tol_mass": 10,
-    "fragment_tol_mode": "ppm",
-    "ion_types": "by",
-    "max_ion_charge": 2,
-    "neutral_losses": {"NH3": -17.026549, "H2O": -18.010565},
-    
-}
-
-def annotate_ion_type(annotation, ion_types="aby"):
-    if annotation.ion_type[0] in ion_types:
+    if annotation.ion_type[0] in ion_types and annotation.ion_type[0] != 'I' :
         if abs(annotation.isotope) == 1:
             iso = "+i" if annotation.isotope > 0 else "-i"
         elif annotation.isotope != 0:
             iso = f"{annotation.isotope:+}i"
         else:
             iso = ""
-        nl = {"-NH3": "*", "-H2O": "o"}.get(annotation.neutral_loss, "")
-        return f"{annotation.ion_type}{iso}{'+' * annotation.charge}{nl}"
-    else:
-        return ""
+        if prec_z == 2:
+            if neutraloss:
+                nl = {"-NH3": "*", "-H2O": "o"}.get(annotation.neutral_loss, "")
+                return f"{annotation.ion_type}{iso}{nl}"
+            else:
+                return f"{annotation.ion_type}{iso}"
+        if prec_z > 2:
+            if neutraloss:
+                nl = {"-NH3": "*", "-H2O": "o"}.get(annotation.neutral_loss, "")
+                return f"{annotation.ion_type}{iso}{'+' * annotation.charge}{nl}"
+            else:
+                return f"{annotation.ion_type}{iso}{'+' * annotation.charge}"
+            
+    elif annotation.ion_type[0] == 'I':
+        if hasattr(annotation, "label") and annot_diag:
+            return annotation.label
+        else:
+            return sup.annotate_ion_type(annotation, "I")
 
-def plot_annotateSpectra(identifier, peptide, precursor_mz, precursor_charge, mz, intensity, output_plot_name):
-    my_spec = sus.MsmsSpectrum(identifier = identifier, precursor_mz=  precursor_mz , precursor_charge = precursor_charge , mz=mz, intensity = intensity)
+    return ""
+
+#plotting function
+#
+def plot_annotateSpectra(identifier: str, 
+    peptide: str, 
+    precursor_mz: float, 
+    precursor_charge: int, 
+    mz: list[float], 
+    intensity: list[float],  
+    output_plot_name: str,
+    tol_mz: float,
+    tol_mode: str,
+    ion_types: str,
+    annot_diag: bool, 
+    nloss: bool):
+
+
+    annotation_settings = {
+    "fragment_tol_mass": float(tol_mz),
+    "fragment_tol_mode": tol_mode,
+    "ion_types": ion_types,
+    "max_ion_charge": 2,
+    "neutral_losses": {"NH3": -17.026549, "H2O": -18.010565},
+    
+    }
+
+    
+    my_spec = sus.MsmsSpectrum(identifier = identifier, 
+                               precursor_mz=  precursor_mz , 
+                               precursor_charge = precursor_charge , 
+                               mz=mz, 
+                               intensity = intensity)
+    
     my_spec = my_spec.filter_intensity(min_intensity = 0.05)
     my_spec.annotate_proforma(proforma_str = peptide, **annotation_settings)
     
-    fig, ax = plt.subplots(figsize=(8,8))
-    sup.spectrum(my_spec, grid=False, ax=ax, annot_fmt=annotate_ion_type, annot_kws={'size': 11})
-    ax.set_title(identifier, fontdict={"fontsize": "xx-large"})
-    ax.set_xlabel("m/z", fontsize=12)  # Set x-axis label font size
-    ax.set_ylabel("Intensity", fontsize=12)  # Set y-axis label font size
-    ax.tick_params(axis="both", which="major", labelsize=10) 
-    ax.spines["right"].set_visible(False)
-    ax.spines["top"].set_visible(False)
-    #ax.text(size = 15)
-    #ax.annotate(fon)
+    sup.colors["I"] = "#455a64"
+    if annot_diag:   # Annotate each peak with diagnostic m/z values
+        sup.colors["I"] = "#03DAC6"
+        diag_mz_annotations = {
+        98.0970: "me1K",
+        112.0757: "foK",
+        126.0917: "acK",
+        140.1075: "prK",
+        154.1226: "buK",
+        152.1070: "crK",
+        156.1019: "laK",
+        170.1176: "hbK"}
+        for i, peak_mz in enumerate(my_spec.mz):
+            for diag_mz, diag_label in diag_mz_annotations.items():
+                # Compute mass difference and check tolerance
+                if float(abs(md := suu.mass_diff(diag_mz, peak_mz, tol_mode == "Da"))) < float(tol_mz):
+                    my_spec.annotation[i]=[
+                        sufa.FragmentAnnotation(
+                            ion_type="I",   
+                            charge=1,
+                            mz_delta=(md, tol_mode),
+                        )
+                    ]
+                    #add new attribute 'label' containing custom label
+                    my_spec.annotation[i][-1].label = diag_label  
+                    break  
+
+
+    fig, ax = plt.subplots()
+
+    sup.spectrum(my_spec, 
+                 grid=False, 
+                 ax=ax, 
+                 annot_fmt= functools.partial(annotate_ion_type, 
+                                              prec_z = my_spec.precursor_charge, 
+                                              ion_types = ion_types,
+                                              annot_diag = annot_diag, 
+                                              neutraloss= nloss), 
+                annot_kws={'size': 12})
+
+
+
+# plot aesthetics
+    ax.set_title(peptide, fontdict={"fontsize": "xx-large"}, pad = 30)
+    ax.set_xlabel("m/z", fontsize=16, weight = 'bold')  # Set x-axis label font size
+    ax.set_ylabel("Intensity", fontsize=16, weight = 'bold')  # Set y-axis label font size
+
+    ## Hide the right and top spines
+    ax.spines[['right', 'top']].set_visible(False)
+
+    ## adjust thickness
+    for axis in ['bottom','left']:
+        ax.spines[axis].set_linewidth(2)
+
+    ax.tick_params(axis="both", which="major", labelsize=14, width = 2, length = 6) 
+
+    peaks = plt.gca()
+    for line in peaks.lines:
+        line.set_linewidth(2) 
+
+    
     if output_plot_name:
-        plt.savefig(output_plot_name, dpi=300)
-        print(f"Plot saved as '{output_plot_name}'")
+        plt.savefig(output_plot_name, dpi=300, transparent=False, bbox_inches="tight")
+        print(f"'{output_plot_name}' spectrum is saved sucessfully.")
+    
     plt.show()
-    plt.close()    
+    plt.close()
+   
 
 
