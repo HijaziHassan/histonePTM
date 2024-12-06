f6a572d8c31cfe137f1d9a382f86ee0d1ac2c0cb
HIJAZI Hassan 280913 <HH280913@intra.cea.fr>
2024-12-06 11:47:53 +0100

    If trypsin does not cut after P. Then this sequence "YRPGTVALR" will appear. I added as I saw some using i for normalization.

--- R/data-sequenceDB.R
+++ R/data-sequenceDB.R
@@ -1,10 +1,10 @@
-#' @title Histone peptides
+#' @title Histone protein and peptide sequence labeling
 #' @description
 #' a named list of proteins including a nested list of sequence, enzyme fragments and labels
 #'
 #' \itemize{
-#'   \item Protein. H3, H4, and iRT
-#'      \item seq. Full amino acid sequence.
+#'   \item Protein. H3, H4, H2A, H2B and iRT
+#'      \item seq. Full amino acid sequence of the core protein and its variants.
 #'      \item Fragments. argC_frags/tryp_frags.
 #'      \item label. Representation of fragments by position in sequence (e.g. "H3 (3-8)" )
 #'
