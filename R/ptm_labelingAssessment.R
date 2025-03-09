fd6fb42b5366d9bf608fde475008d3c05e4db26e
HIJAZI Hassan 280913 <HH280913@intra.cea.fr>
2025-03-09 23:02:49 +0100

    major modifications. now it does not plot the total, as it is informative. it add UnderOverlabeled and now under is only underlabeled and over is only overlabeled. If both, the it is underoverlabel. It has a built-in peptides of no sequence was passed. 'All' is when you need to analyze all the peptides in the dataset. It now accepts more than one peptide to be passed.

--- R/ptm_labelingAssessment.R
+++ R/ptm_labelingAssessment.R
@@ -3,27 +3,33 @@
 
 
 
-#' Assessing (over)Labeling Effeciency
+#' Assessing (over)Labeling Efficiency
 #'
-#' Some time overlabeling is very abundant which decrease sensitivity and affect quantification accuracy.
+#' Sometimes overlabeling is very abundant which decrease sensitivity and affect quantification accuracy.
 #'
-#' @param df Your dataset
+#' @param df Proteomics results dataset.
 #' @param seq_col column containing bare peptide sequences
-#' @param seq sequence to base the assessment on it
+#' @param seq sequence(s) to base the assessment on. Multiple sequences should be passed as a character vector.
+#' If empty or `NULL` (default), five H3 and H4 built-in sequences will be analyzed.
+#' If "All", then all the unique sequence in the `seq_col` will be analyzed.
 #' @param ptm_col column containing peptide ptm information (for overTMA: TMAyl, tmaNt, and tmame1, tma;, for overProp: Propionyl, prNt, pr). Only
-#'two representations of PTMs can be recognized to filter upon: Propionly (K12) or K27bu-S32pr ....)
+#'two representations of PTMs can be recognized to filter upon: Propionyl (K12) or K27bu-S32pr ....)
 #' @param int_col sample columns containing intensity values
+#' @param save_plot bool; `FALSE` (default). Save or not the plot to the desktop.`
 #' @param plot_title A title for the plot
 #' @importFrom stringr str_detect str_count str_remove
 #' @importFrom tidyr pivot_longer
-#' @importFrom dplyr filter mutate summarise across select distinct if_else cur_column pull bind_rows
+#' @importFrom dplyr filter mutate summarise across select distinct if_else cur_column pull bind_rows case_when
+#' @importFrom purrr map
+#' @importFrom tibble tibble
+#' @importFrom cli cli_alert_info cli_alert_warning
 #' @import ggplot2
 #'
 #' @return list of two dataframes (raw and normalized values) and a bar plot.
 #' @export
 
 
-ptm_labelingAssessment <- function(df, seq_col, seq, ptm_col, int_col, plot_title= ""){
+ptm_labelingAssessment <- function(df, seq_col, seq = NULL, ptm_col, int_col, save_plot = FALSE, plot_title= ""){
 
 
 
@@ -42,113 +48,110 @@ ptm_labelingAssessment <- function(df, seq_col, seq, ptm_col, int_col, plot_titl
 
 
 
-  # filter data by removing duplications, selecting the sequence of interest
-  df_filtered <- df |>
-    dplyr::select({{seq_col}}, {{ptm_col}}, {{int_col}}) |>
-    dplyr::filter(stringr::str_detect({{seq_col}}, {{seq}})) |>
-    dplyr::distinct(dplyr::across({{int_col}}), .keep_all = TRUE)
+    if (identical(seq, "All")) {
+      seq <- unique(df[[as.character(substitute(seq_col))]])
+    } else if (is.null(seq) || length(seq) == 0) {
+      seq <- c('ISGLIYEETR', 'YRPGTVALREIR', 'KQLATKAAR', 'DNIQGITKPAIR', 'DAVTYTEHAKR', 'KSTGGKAPR', 'KSAP.TGGVKKPHR')
+    }
 
-#### Calculate CVs #####
-
-if(nrow(df_filtered)>1){
-
-
-#### Label the data #####
-
-  df_tagged <- df_filtered |>
-    dplyr::mutate(isOverLab = stringr::str_detect({{ptm_col}}, regex_OverLab),
-                  isNonLabeledme1 = stringr::str_detect({{ptm_col}}, regex_nonLabMe1),
-                  isFullyModified= dplyr::if_else(stringr::str_count({{seq_col}}, "K") == stringr::str_count({{ptm_col}}, regex_Kmod),
-                                                  "TRUE",
-                                                  "FALSE"),
-                  isNterm = stringr::str_detect({{ptm_col}}, regex_Nterm ))
+    df_filtered <- df |>
+      dplyr::select({{seq_col}}, {{ptm_col}}, {{int_col}}) |>
+      dplyr::filter(stringr::str_detect({{seq_col}}, paste(seq, collapse = "|"))) |>
+      dplyr::distinct(dplyr::across({{int_col}}), .keep_all = TRUE)
 
 
-##### Isolate and sum each labeled group #####
-  df_total <- df_tagged |>
-    dplyr::summarise(dplyr::across({{int_col}},  ~sum(.x, na.rm = T))) |>
-    dplyr::mutate(label = 'Total')
+    if (nrow(df_filtered) == 0) {
+      cli::cli_alert_warning("None of the specified sequences were found in the data.")
+      return(NULL)
+    }
 
 
 
-  df_underlab <- df_tagged |>
-                #monomethyl unlabeled OR unlabeled K OR not N-term modified
-    dplyr::filter(isNonLabeledme1 == TRUE | isFullyModified == FALSE | isNterm == FALSE) |>
-    dplyr::summarise(dplyr::across({{int_col}},  ~sum(.x, na.rm = T))) |>
-    dplyr::mutate(label = 'UnderLabeled')
+    df_results <- purrr::map(seq, function(current_seq) {
+      df_seq <- df_filtered |>
+        dplyr::filter(stringr::str_detect({{seq_col}}, current_seq))
 
-  df_overlab <- df_tagged |>
-    dplyr::filter(isOverLab == TRUE) |>
-    dplyr::summarise(dplyr::across({{int_col}},  ~sum(.x, na.rm = T))) |>
-    dplyr::mutate(label = 'OverLabeled')
+      if (nrow(df_seq) < 1) {
+        cli::cli_alert_info('The sequence {.val {current_seq}} is not found in your {.arg seq_col} column.')
+        return(tibble::tibble())
+      }
 
-
-  df_desired <- df_tagged |>
-    dplyr::filter(isNterm == TRUE ,
-                  isOverLab == FALSE,
-                  isNonLabeledme1 == FALSE,
-                  isFullyModified == TRUE) |>
-    dplyr::summarise(dplyr::across({{int_col}},  ~sum(.x, na.rm = T))) |>
-    dplyr::mutate(label = 'Desired')
-
-
-
-  df_total = dplyr::bind_rows(df_total, df_underlab, df_overlab, df_desired) |>
-    mutate(seq_analyzed = seq)
+      df_tagged <- df_seq |>
+        dplyr::mutate(
+          isOverLab = stringr::str_detect({{ptm_col}}, regex_OverLab),
+          isNonLabeledme1 = stringr::str_detect({{ptm_col}}, regex_nonLabMe1),
+          isFullyModified = dplyr::if_else(
+            stringr::str_count({{seq_col}}, "K") == stringr::str_count({{ptm_col}}, regex_Kmod),
+            TRUE, FALSE
+          ),
+          isNterm = stringr::str_detect({{ptm_col}}, regex_Nterm)
+        )
 
 
-  df_total_norm <- df_total |>
-    dplyr::mutate( dplyr::across(.cols =  {{int_col}},
-                                 .fns = ~ .*100 / df_total |>
-                                   dplyr::filter(label == 'Total') |>
-                                   dplyr::pull(dplyr::cur_column())
-    )
-    )
+      df_total <- df_tagged |>
+        dplyr::summarise(dplyr::across({{int_col}}, ~sum(.x, na.rm = TRUE)))
 
 
-  #Change angle of x-axis labels according to sample number
 
-  sample_number <- length(colnames(df_filtered))-2
+      df_all <- df_tagged |>
+        dplyr::mutate(
+          label = dplyr::case_when(
+            isNonLabeledme1 | !isFullyModified | !isNterm & !isOverLab ~ 'UnderLabeled',
+            isOverLab & !(isNonLabeledme1 | !isFullyModified | !isNterm) ~ 'OverLabeled',
+            isNterm & !isOverLab & !isNonLabeledme1 & isFullyModified ~ 'Desired',
+            .default = 'UnderOverLabeled'
+          )
+        ) |>
 
-  angle = ifelse(sample_number < 8, 0, 45)
+        dplyr::summarise(dplyr::across({{int_col}}, ~sum(.x, na.rm = TRUE)), .by = label) |>
+        dplyr::mutate(seq_analyzed = current_seq)
 
-
-  p <- df_total_norm |>
-    dplyr::mutate(label = factor(label, levels= c('Total', 'Desired', 'OverLabeled', 'UnderLabeled'))) |>
-    tidyr::pivot_longer( cols = {{int_col}},
-                         #names_pattern = 'abundance_(.*)',
-                         names_to = 'sample',
-                         values_to = 'intensity' ) |>
-    dplyr::mutate(sample= stringr::str_remove(sample, 'abundance_')) |>
-    ggplot2::ggplot(aes(x= sample, y= intensity, fill= label))+
-    ggplot2::labs(y= "Relative Intensity (%)", fill= "", x= "", title= plot_title)+
-    ggplot2::geom_col(position= ggplot2::position_dodge2(preserve= 'single'))+
-    ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0, 0.01)))+
-    ggplot2::scale_fill_brewer(palette = "Dark2")+
-    ggplot2::theme_classic(base_size = 14)+
-    ggplot2::theme(legend.position = "bottom", legend.direction = "horizontal",
-                   legend.text = ggplot2::element_text(face= "bold"),
-                   axis.text.x = ggplot2::element_text(angle = angle, vjust = 0.5),
-                   axis.line = ggplot2::element_line(linewidth = 1.2),
-                   axis.text = element_text(face= 'bold', color = 'black')
-                   )
+      df_all_norm <- df_all |>
+        dplyr::mutate(
+          dplyr::across(
+            .cols = {{int_col}},
+            .fns = ~ . * 100 / df_total |> dplyr::pull(dplyr::cur_column())
+          )
+        )
 
 
-  #what about misclevages
-  #make sure how underprop and overprop are related.
-  #adapt it to take more than one sequence.
 
-  return(list(raw_data = df_total, normalized_data = df_total_norm, plot= p))
+      # Plot for each sequence
 
-    }else if (nrow(df_filtered) == 0){
+      plot <- df_all_norm |>
+        dplyr::mutate(label = factor(label, levels= c('Total', 'Desired', 'OverLabeled', 'UnderLabeled', 'UnderOverLabeled'))) |>
+        tidyr::pivot_longer(
+          cols = {{int_col}},
+          names_to = 'sample',
+          values_to = 'intensity'
+        ) |>
+        dplyr::mutate(sample = stringr::str_remove(sample, 'abundance_')) |> # specific for Proline software
+        ggplot2::ggplot(ggplot2::aes(x= sample, y= intensity, fill= label)) +
+        ggplot2::labs(y= "Relative Intensity (%)", fill= "", x= "", title= plot_title) +
+        ggplot2::geom_col(position= ggplot2::position_dodge2(preserve= 'single')) +
+        ggplot2::geom_hline(yintercept = 100,  linetype = "dotted", color= "#DEDEDE")+
+        ggplot2::scale_y_continuous(expand = ggplot2::expansion(mult = c(0, 0.01))) +
+        ggplot2::scale_fill_brewer(palette = "Dark2") +
+        ggplot2::theme_classic(base_size = 14) +
+        ggplot2::theme(
+          legend.position = "bottom", legend.direction = "horizontal",
+          legend.text = ggplot2::element_text(face= "bold"),
+          axis.text.x = ggplot2::element_text(angle = 45, vjust = 0.5),
+          axis.line = ggplot2::element_line(linewidth = 1.2),
+          axis.text = ggplot2::element_text(face= 'bold', color = 'black')
+        )
 
-  cli::cli_alert_info('The sequence {.val {seq}} is not found in your {.val {deparse(substitute(seq_col))}} column.')
+      if(save_plot){
+      cat(paste0('Plotting: ', current_seq))
+      filename <- paste0('overlabassess_', current_seq, ".png")
+      ggplot2::ggsave(filename, plot, width = 8, height = 6)
+      }
+      return(df_all_norm)
 
-      }else{
+    }) |> dplyr::bind_rows()
 
-         return(NULL)
-
+    return(df_results)
   }
 
-}
+
 
